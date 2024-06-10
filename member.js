86b6ed22a18805c986829e5e1f0f3a460fb1776b
Ohazulike Chukwuebuka Francis <106169180+Godwinn22@users.noreply.github.com>
2024-06-10 12:33:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+  return [
+    {
+      id: 1,
+      name: "HTML",
+      percent: 90,
+    },
+    {
+      id: 2,
+      name: "CSS",
+      percent: 80,
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      percent: 85,
+    },
+    {
+      id: 4,
+      name: "React",
+      percent: 70,
+    },
+    {
+      id: 5,
+      name: "Node",
+      percent: 75,
+    },
+  ];
+}
